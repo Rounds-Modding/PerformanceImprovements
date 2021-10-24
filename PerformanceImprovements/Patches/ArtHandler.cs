1d5e69353f1d5b60ea5f88a3ef45f93cc9963dc5
pdcook <qphysicscook@gmail.com>
2021-10-24 15:27:35 -0400

    added removepostfx stuff

---
+++ PerformanceImprovements/Patches/ArtHandler.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using HarmonyLib;
+using UnityEngine;
+using UnboundLib;
+
+namespace PerformanceImprovements.Patches
+{
+	[HarmonyPatch(typeof(ArtHandler), "NextArt")]
+	internal class ArtHandler_Patch
+	{
+		private static GameObject Rendering => GameObject.Find("/Game/Visual/Rendering ");
+		private static GameObject FrontParticles => Rendering?.transform?.GetChild(1)?.gameObject;
+		private static GameObject BackParticles => Rendering?.transform?.GetChild(0)?.gameObject;
+		private static GameObject Light => Rendering?.transform?.GetChild(3)?.GetChild(0)?.gameObject;
+
+		private static Color staticGunColor = new Color(0.25f, 0.25f, 0.25f, 1f);
+
+		private static void Postfix()
+		{
+			foreach (ParticleSystem particleSystem in UnityEngine.Object.FindObjectsOfType<ParticleSystem>())
+			{
+				if (particleSystem.gameObject.name.Contains("Skin_Player"))
+				{
+					particleSystem.SetPropertyValue("enableEmission", !PerformanceImprovements.DisablePlayerParticles.Value);
+
+					Player player = particleSystem.gameObject.GetComponentInParent<Player>();
+					Gun gun = player.GetComponent<Holding>().holdable.GetComponent<Gun>();
+					GameObject spring = gun.gameObject.transform.GetChild(1).gameObject;
+					GameObject handle = spring.transform.GetChild(2).gameObject;
+					GameObject barrel = spring.transform.GetChild(3).gameObject;
+
+					handle.GetComponent<SpriteMask>().enabled = !PerformanceImprovements.DisablePlayerParticles.Value;
+					handle.GetComponent<SpriteRenderer>().enabled = PerformanceImprovements.DisablePlayerParticles.Value;
+					handle.GetComponent<SpriteRenderer>().color = staticGunColor;
+					barrel.GetComponent<SpriteMask>().enabled = !PerformanceImprovements.DisablePlayerParticles.Value;
+					barrel.GetComponent<SpriteRenderer>().enabled = PerformanceImprovements.DisablePlayerParticles.Value;
+					barrel.GetComponent<SpriteRenderer>().color = staticGunColor;
+				}
+			}
+			BackParticles?.SetActive(!PerformanceImprovements.DisableBackgroundParticles.Value);
+			FrontParticles?.SetActive(!PerformanceImprovements.DisableMapParticles.Value);
+			Light?.SetActive(!PerformanceImprovements.DisableOverheadLightAndShadows.Value);
+			/*
+			GameObject backgroundpart = GameObject.Find("BackgroudParticles");
+			if (backgroundpart != null)
+			{
+				backgroundpart.SetActive(!PerformanceImprovements.DisableMapParticles.Value);
+			}
+			GameObject foregroundpart = GameObject.Find("FrontParticles");
+			if (foregroundpart != null)
+			{
+				foregroundpart.SetActive(!PerformanceImprovements.DisableMapParticles.Value);
+			}
+			GameObject light = GameObject.Find("Light");
+			if (light != null)
+			{
+				light.SetActive(!PerformanceImprovements.DisableOverheadLightAndShadows.Value);
+			}*/
+		}
+	}
+}
--- PerformanceImprovements/PerformanceImprovements.cs
+++ PerformanceImprovements/PerformanceImprovements.cs
@@ -29,13 +29,37 @@ namespace PerformanceImprovements
         private const string ModName = "Performance Improvements";
         private const string CompatibilityModName = "PerformanceImprovements";
 
-        internal static bool GameInProgress = false;
+        private static bool _GameInProgress = false;
+        internal static bool GameInProgress
+        {
+            get
+            {
+                if (GM_Test.instance != null && GM_Test.instance.gameObject != null)
+                {
+                    return (_GameInProgress || GM_Test.instance.gameObject.activeInHierarchy);
+                }
+                else
+                {
+                    return _GameInProgress;
+                }
+
+            }
+            set
+            {
+                _GameInProgress = value;
+            }
+        }
 
         internal static AssetBundle Assets;
 
         public static ConfigEntry<bool> DisableCardParticleAnimations;
         public static ConfigEntry<int> NumberOfGeneralParticles;
-        public static ConfigEntry<bool> ToggleSimpleSkins;
+        public static ConfigEntry<bool> DisablePlayerParticles;
+        public static ConfigEntry<bool> DisableMapParticles;
+        public static ConfigEntry<bool> DisableBackgroundParticles;
+        public static ConfigEntry<bool> DisableOverheadLightAndShadows;
+        public static ConfigEntry<float> ScreenShakeStrength;
+        public static ConfigEntry<float> ChromaticAberrationStrength;
 
 
         private void Awake()
@@ -43,7 +67,12 @@ namespace PerformanceImprovements
             // bind configs with BepInEx
             DisableCardParticleAnimations = Config.Bind(CompatibilityModName, "Disable Card Particle System Animations", true);
             NumberOfGeneralParticles = Config.Bind(CompatibilityModName, "Number of General Particles", 100);
-            ToggleSimpleSkins = Config.Bind(CompatibilityModName, "Use Simple Player Skins", true);
+            DisablePlayerParticles = Config.Bind(CompatibilityModName, "Disable player particles", true);
+            DisableMapParticles = Config.Bind(CompatibilityModName, "Disable map particles", true);
+            DisableBackgroundParticles = Config.Bind(CompatibilityModName, "Disable background particles", true);
+            DisableOverheadLightAndShadows = Config.Bind(CompatibilityModName, "Disable overhead light and shadows", true);
+            ScreenShakeStrength = Config.Bind(CompatibilityModName, "Screen shake strength from 0 to 1", 0f);
+            ChromaticAberrationStrength = Config.Bind(CompatibilityModName, "Chromatic Aberration Strength from 0 to 1", 0f);
             // apply patches
             new Harmony(ModId).PatchAll();
         }
@@ -70,6 +99,9 @@ namespace PerformanceImprovements
 
                 orig(self);
             };
+            // add modifications to GameFeel
+            On.Screenshaker.OnGameFeel += this.Screenshaker_OnGameFeel;
+            On.ChomaticAberrationFeeler.OnGameFeel += this.ChomaticAberrationFeeler_OnGameFeel;
         }
         private static IEnumerator SetGameInProgress(bool inProgress)
         {
@@ -91,13 +123,44 @@ namespace PerformanceImprovements
             MenuHandler.CreateSlider("Max number of particles for GeneralParticleSystems", menu, 30, 10f, 100f, NumberOfGeneralParticles.Value, NumPartsChanged, out Slider slider, true);
             void SimpleSkinChanged(bool val)
             {
-                ToggleSimpleSkins.Value = val;
-                if (PerformanceImprovements.GameInProgress)
-                {
-                    PlayerSkinHandlerPatchStart.ForceSwitch();
-                }
+                DisablePlayerParticles.Value = val;
             }
-            MenuHandler.CreateToggle(ToggleSimpleSkins.Value, "Use simple player skins", menu, SimpleSkinChanged, 30);
+            MenuHandler.CreateToggle(DisablePlayerParticles.Value, "Use simple player skins", menu, SimpleSkinChanged, 30);
+            void MapParticlesChanged(bool val)
+            {
+                DisableMapParticles.Value = val;
+            }
+            MenuHandler.CreateToggle(DisableMapParticles.Value, "Disable Map Particle Effects", menu, MapParticlesChanged, 30);
+            void BackParticlesChanged(bool val)
+            {
+                DisableBackgroundParticles.Value = val;
+            }
+            MenuHandler.CreateToggle(DisableBackgroundParticles.Value, "Disable Background Particle Effects", menu, BackParticlesChanged, 30);
+            void LightChanged(bool val)
+            {
+                DisableOverheadLightAndShadows.Value = val;
+            }
+            MenuHandler.CreateToggle(DisableOverheadLightAndShadows.Value, "Disable Overhead light and shadows", menu, LightChanged, 30);
+            void ShakeChanged(float val)
+            {
+                ScreenShakeStrength.Value = val / 100f;
+            }
+            void AberrationChanged(float val)
+            {
+                ChromaticAberrationStrength.Value = val / 100f;
+            }
+            MenuHandler.CreateSlider("Screen Shake Strength", menu, 30, 0f, 100f, (int)(100f*ScreenShakeStrength.Value), ShakeChanged, out Slider _, true);
+            MenuHandler.CreateSlider("Chromatic Aberration Strength", menu, 30, 0f, 100f, (int)(100f*ChromaticAberrationStrength.Value), AberrationChanged, out Slider _, true);
+        }
+
+        private void Screenshaker_OnGameFeel(On.Screenshaker.orig_OnGameFeel orig, global::Screenshaker self, Vector2 feelDirection)
+        {
+            orig(self, feelDirection * (float)(ScreenShakeStrength.Value));
+        }
+
+        private void ChomaticAberrationFeeler_OnGameFeel(On.ChomaticAberrationFeeler.orig_OnGameFeel orig, global::ChomaticAberrationFeeler self, Vector2 feelDirection)
+        {
+            orig(self, feelDirection * (float)(ChromaticAberrationStrength.Value));
         }
     }
 }
