60d9efb67dbe72ff79a41f00689e6fa88184ed20
pdcook <qphysicscook@gmail.com>
2021-10-24 00:44:31 -0400

    initial commit

---
+++ PerformanceImprovements/Patches/GeneralParticleSystem.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using HarmonyLib;
+using UnboundLib;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace PerformanceImprovements.Patches
+{
+    [HarmonyPatch(typeof(GeneralParticleSystem),"Play")]
+    class GeneralParticleSystemPatchPlay
+    {
+
+		private const float staticRandomRotation = 45f;
+		private const float staticRandomXPos = 100f;
+		private const float staticRandomYPos = 200f;
+
+        private static bool Prefix(GeneralParticleSystem __instance)
+        {
+            if (PerformanceImprovements.DisableCardParticleAnimations.Value && PerformanceImprovements.GameInProgress)
+            {
+                __instance.InvokeMethod("Init", new object[] { });
+				int num = (int)UnityEngine.Mathf.Clamp(6, 0, PerformanceImprovements.NumberOfGeneralParticles.Value);
+				for (int i = 0; i < num; i++)
+                {
+					CreateParticleStatic(__instance, i/__instance.duration);
+                }
+                return false;
+            }
+            return true;
+        }
+		private static void CreateParticleStatic(GeneralParticleSystem instance, float currentAnimationTime)
+		{
+			GameObject spawned = ((ObjectPool)instance.GetFieldValue("particlePool")).GetObject();
+			float counter = UnityEngine.Random.Range(0f, instance.particleSettings.lifetime);
+			float t = instance.particleSettings.lifetime;
+			Vector3 startSize = spawned.transform.localScale;
+			Vector3 modifiedStartSize = spawned.transform.localScale * instance.particleSettings.size * instance.sizeMultiplierOverTime.Evaluate(currentAnimationTime * (float)instance.GetFieldValue("sizeMultiplierOverTimeAnimationCurveLength"));
+			Image img = spawned.GetComponent<Image>();
+			Color startColor = Color.magenta;
+			if (img)
+			{
+				startColor = img.color;
+			}
+			if (img)
+			{
+				float value = UnityEngine.Random.value;
+				if (instance.particleSettings.color != Color.magenta)
+				{
+					img.color = instance.particleSettings.color;
+				}
+				if (instance.particleSettings.randomColor != Color.magenta)
+				{
+					img.color = Color.Lerp(img.color, instance.particleSettings.randomColor, value);
+				}
+				if (!instance.particleSettings.singleRandomValueColor)
+				{
+					value = UnityEngine.Random.value;
+				}
+				if (instance.particleSettings.randomAddedColor != Color.black)
+				{
+					img.color += Color.Lerp(Color.black, instance.particleSettings.randomAddedColor, value);
+				}
+				if (!instance.particleSettings.singleRandomValueColor)
+				{
+					value = UnityEngine.Random.value;
+				}
+				if (instance.particleSettings.randomAddedSaturation != 0f || instance.saturationMultiplier != 1f)
+				{
+					float h;
+					float num;
+					float v;
+					Color.RGBToHSV(img.color, out h, out num, out v);
+					num += value * instance.particleSettings.randomAddedSaturation;
+					num *= instance.saturationMultiplier;
+					img.color = Color.HSVToRGB(h, num, v);
+				}
+			}
+			spawned.transform.Rotate(instance.transform.forward * instance.particleSettings.rotation);
+			spawned.transform.Rotate(instance.transform.forward * UnityEngine.Random.Range(-staticRandomRotation, staticRandomRotation));
+			spawned.transform.localPosition = Vector3.zero;
+			spawned.transform.position += instance.transform.up * UnityEngine.Random.Range(-staticRandomYPos, staticRandomYPos);
+			spawned.transform.position += instance.transform.right * UnityEngine.Random.Range(-staticRandomXPos, staticRandomXPos);
+			spawned.transform.position += instance.transform.forward * UnityEngine.Random.Range(-0.1f, 0.1f);
+			spawned.transform.localPosition += instance.transform.up * UnityEngine.Random.Range(-staticRandomYPos, staticRandomYPos);
+			spawned.transform.localPosition += instance.transform.right * UnityEngine.Random.Range(-staticRandomXPos, staticRandomXPos);
+			spawned.transform.localPosition += instance.transform.forward * UnityEngine.Random.Range(-0.1f, 0.1f);
+			if (instance.particleSettings.sizeOverTime.keys.Length > 1)
+			{
+				spawned.transform.localScale = modifiedStartSize * instance.particleSettings.sizeOverTime.Evaluate(counter / t * (float)instance.GetFieldValue("sizeOverTimeAnimationCurveLength"));
+			}
+			float num2 = instance.particleSettings.alphaOverTime.Evaluate(counter / t * (float)instance.GetFieldValue("alphaOverTimeAnimationCurveLength"));
+			if (img && img.color.a != num2)
+			{
+				img.color = new Color(img.color.r, img.color.g, img.color.b, num2);
+			}
+			return;
+			/*
+			while (counter < t)
+			{
+
+				if (instance.particleSettings.sizeOverTime.keys.Length > 1)
+				{
+					spawned.transform.localScale = modifiedStartSize * instance.particleSettings.sizeOverTime.Evaluate(counter / t * (float)instance.GetFieldValue("sizeOverTimeAnimationCurveLength"));
+				}
+				float num2 = instance.particleSettings.alphaOverTime.Evaluate(counter / t * (float)instance.GetFieldValue("alphaOverTimeAnimationCurveLength"));
+				if (img && img.color.a != num2)
+				{
+					img.color = new Color(img.color.r, img.color.g, img.color.b, num2);
+				}
+				counter += (instance.useTimeScale ? TimeHandler.deltaTime : Time.unscaledDeltaTime) * (instance.simulationSpeed * instance.simulationSpeedMultiplier);
+				yield return null;
+			}
+
+			if (img)
+			{
+				img.color = startColor;
+			}
+			spawned.transform.localScale = startSize;
+				
+			
+			//((ObjectPool)instance.GetFieldValue("particlePool")).ReleaseObject(spawned);
+			yield break;*/
+		}
+	}
+    [HarmonyPatch(typeof(ObjectPool),MethodType.Constructor)]
+	[HarmonyPatch(new Type[] {typeof(GameObject), typeof(int), typeof(Transform)})]
+    class ObjectPoolPatchConstructor
+    {
+        private static void Prefix(GameObject prefab, ref int initSpawn, Transform parent)
+        {
+			if (PerformanceImprovements.GameInProgress)
+            {
+				initSpawn = (int)UnityEngine.Mathf.Clamp(initSpawn, 0f, PerformanceImprovements.NumberOfGeneralParticles.Value);
+			}
+		}
+    }
+}
---
+++ PerformanceImprovements/PerformanceImprovements.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using BepInEx;
+using BepInEx.Configuration;
+using UnboundLib;
+using HarmonyLib;
+using UnityEngine;
+using Jotunn.Utils;
+using System.Runtime.CompilerServices;
+using System.Reflection;
+using UnboundLib.Utils.UI;
+using TMPro;
+using UnityEngine.Events;
+using UnityEngine.UI;
+using System.Linq;
+using System.Collections.Generic;
+using On;
+using UnboundLib.GameModes;
+using System.Collections;
+using PerformanceImprovements.Patches;
+
+namespace PerformanceImprovements
+{
+    [BepInDependency("com.willis.rounds.unbound", BepInDependency.DependencyFlags.HardDependency)]
+    [BepInPlugin(ModId, ModName, "0.0.0")]
+    [BepInProcess("Rounds.exe")]
+    public class PerformanceImprovements : BaseUnityPlugin
+    {
+        private const string ModId = "pykess-and-ascyst.rounds.plugins.performanceimprovements";
+        private const string ModName = "Performance Improvements";
+        private const string CompatibilityModName = "PerformanceImprovements";
+
+        internal static bool GameInProgress = false;
+
+        internal static AssetBundle Assets;
+
+        public static ConfigEntry<bool> DisableCardParticleAnimations;
+        public static ConfigEntry<int> NumberOfGeneralParticles;
+        public static ConfigEntry<bool> ToggleSimpleSkins;
+
+
+        private void Awake()
+        {
+            // bind configs with BepInEx
+            DisableCardParticleAnimations = Config.Bind(CompatibilityModName, "Disable Card Particle System Animations", true);
+            NumberOfGeneralParticles = Config.Bind(CompatibilityModName, "Number of General Particles", 100);
+            ToggleSimpleSkins = Config.Bind(CompatibilityModName, "Use Simple Player Skins", true);
+            // apply patches
+            new Harmony(ModId).PatchAll();
+        }
+        private void Start()
+        {
+            // load assets
+            //PerformanceImprovements.Assets = AssetUtils.LoadAssetBundleFromResources("performance", typeof(PerformanceImprovements).Assembly);
+
+            // add credits
+            Unbound.RegisterCredits(ModName, new string[] { "Pykess", "Ascyst" }, new string[] { "github", "Buy Pykess a coffee", "Buy Ascyst a coffee" }, new string[] { "https://github.com/Rounds-Modding/PerformanceImprovements", "https://www.buymeacoffee.com/Pykess", "https://www.buymeacoffee.com/Ascyst" });
+
+            // add GUI to modoptions menu
+            Unbound.RegisterMenu(ModName, () => { }, this.NewGUI, null, true);
+
+            // register as client-side
+            Unbound.RegisterClientSideMod(ModId);
+
+            GameModeManager.AddHook(GameModeHooks.HookGameStart, (gm) => SetGameInProgress(true));
+
+            // reset GameInProgress
+            On.MainMenuHandler.Awake += (orig, self) =>
+            {
+                PerformanceImprovements.GameInProgress = false;
+
+                orig(self);
+            };
+        }
+        private static IEnumerator SetGameInProgress(bool inProgress)
+        {
+            PerformanceImprovements.GameInProgress = inProgress;
+            yield break;
+        }
+        private void NewGUI(GameObject menu)
+        {
+            MenuHandler.CreateText(ModName + " Options", menu, out TextMeshProUGUI _, 60);
+            void CardPartAnimChanged(bool val)
+            {
+                DisableCardParticleAnimations.Value = val;
+            }
+            MenuHandler.CreateToggle(DisableCardParticleAnimations.Value, "Disable Card Particle Animations", menu, CardPartAnimChanged, 30);
+            void NumPartsChanged(float val)
+            {
+                NumberOfGeneralParticles.Value = (int)val;
+            }
+            MenuHandler.CreateSlider("Max number of particles for GeneralParticleSystems", menu, 30, 10f, 100f, NumberOfGeneralParticles.Value, NumPartsChanged, out Slider slider, true);
+            void SimpleSkinChanged(bool val)
+            {
+                ToggleSimpleSkins.Value = val;
+                if (PerformanceImprovements.GameInProgress)
+                {
+                    PlayerSkinHandlerPatchStart.ForceSwitch();
+                }
+            }
+            MenuHandler.CreateToggle(ToggleSimpleSkins.Value, "Use simple player skins", menu, SimpleSkinChanged, 30);
+        }
+    }
+}
